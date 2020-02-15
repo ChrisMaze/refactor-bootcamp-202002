445aec458c77d9b2d103ae1840992d1a36198a98
Zerong <zerong.ma@thoughtworks.com>
2020-02-15 20:15:47 +0800

    feat: just return 1 when I get the fibonacci number with position 1 and 2

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
@@ -1,15 +1,20 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciCalculatorTest {
     FibonacciCalculator fibonacciCalculator = new FibonacciCalculator();
 
     @Test
     public void should_return_1_when_calculate_given_position_is_1() {
-        int fibonacciNumber = fibonacciCalculator.getFibonacciByPosition(1);
-        Assertions.assertEquals(1,fibonacciNumber);
+        assertEquals(1, fibonacciCalculator.getFibonacciByPosition(1));
+    }
+
+    @Test
+    public void should_return_1_when_calculate_given_position_is_2() {
+        assertEquals(1, fibonacciCalculator.getFibonacciByPosition(2));
     }
 }
