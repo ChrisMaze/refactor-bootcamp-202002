d7b55811394ab605270eabb4b6319e73c23e304a
Zerong <zerong.ma@thoughtworks.com>
2020-02-15 20:46:13 +0800

    feat: should throw exception when position is 0

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
@@ -2,6 +2,8 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class FibonacciCalculator {
     public long getFibonacciByPosition(int position) {
+        if (position == 0)
+            throw new IllegalArgumentException();
         if (position == 1 || position == 2)
             return 1;
 
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
@@ -4,6 +4,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FibonacciCalculatorTest {
     FibonacciCalculator fibonacciCalculator = new FibonacciCalculator();
@@ -20,21 +21,28 @@ public class FibonacciCalculatorTest {
 
     @Test
     public void should_return_2_when_calculate_given_position_is_3() {
-        assertEquals(2,fibonacciCalculator.getFibonacciByPosition(3));
+        assertEquals(2, fibonacciCalculator.getFibonacciByPosition(3));
     }
 
     @Test
     public void should_return_3_when_calculate_given_position_is_4() {
-        assertEquals(3,fibonacciCalculator.getFibonacciByPosition(4));
+        assertEquals(3, fibonacciCalculator.getFibonacciByPosition(4));
     }
 
     @Test
     public void should_return_5_when_calculate_given_position_is_5() {
-        assertEquals(5,fibonacciCalculator.getFibonacciByPosition(5));
+        assertEquals(5, fibonacciCalculator.getFibonacciByPosition(5));
     }
 
     @Test
     public void should_return_12586269025L_when_calculate_given_position_is_50() {
-        assertEquals(12586269025L,fibonacciCalculator.getFibonacciByPosition(50));
+        assertEquals(12586269025L, fibonacciCalculator.getFibonacciByPosition(50));
+    }
+
+    @Test
+    public void should_throw_when_calculate_given_position_is_0() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            fibonacciCalculator.getFibonacciByPosition(0);
+        });
     }
 }
