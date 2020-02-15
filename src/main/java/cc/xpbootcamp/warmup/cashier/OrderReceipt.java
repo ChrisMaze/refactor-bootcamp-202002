ce3b654c44dcb0727b14ae2fbb350478673c2c5e
Zerong <zerong.ma@thoughtworks.com>
2020-02-15 21:59:13 +0800

    refactor: code smell - Mysterious; refactor - extract constant

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -7,10 +7,11 @@ import java.util.concurrent.atomic.AtomicReference;
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- *
  */
 public class OrderReceipt {
     public static final double taxRate = .10;
+    public static final char tabs = '\t';
+    public static final char newLine = '\n';
     private Order order;
 
     public OrderReceipt(Order order) {
@@ -36,22 +37,22 @@ public class OrderReceipt {
     }
 
     private void PrintTotalAmount(StringBuilder output, AtomicReference<Double> totalAmount) {
-        output.append("Total Amount").append('\t').append(totalAmount.get());
+        output.append("Total Amount").append(tabs).append(totalAmount.get());
     }
 
     private void PrintsTheStateTax(StringBuilder output, AtomicReference<Double> totalSalesTax) {
-        output.append("Sales Tax").append('\t').append(totalSalesTax.get());
+        output.append("Sales Tax").append(tabs).append(totalSalesTax.get());
     }
 
     private void printReceiptBody(StringBuilder output, LineItem lineItem) {
         output.append(lineItem.getDescription());
-        output.append('\t');
+        output.append(tabs);
         output.append(lineItem.getPrice());
-        output.append('\t');
+        output.append(tabs);
         output.append(lineItem.getQuantity());
-        output.append('\t');
+        output.append(tabs);
         output.append(lineItem.totalAmount());
-        output.append('\n');
+        output.append(newLine);
     }
 
     private void PrintReceiptHeaders(StringBuilder output) {
