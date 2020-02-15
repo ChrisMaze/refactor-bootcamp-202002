90b91bfbbb08b29c34386034db8f3317bc746f19
Zerong <zerong.ma@thoughtworks.com>
2020-02-15 21:40:49 +0800

    refactor: code smell - loop; refactor - replace loop with pipeline

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,5 +1,7 @@
 package cc.xpbootcamp.warmup.cashier;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * OrderReceipt prints the details of order including customer name, address, description, quantity,
  * price and amount. It also calculates the sales tax @ 10% and prints as part
@@ -17,38 +19,39 @@ public class OrderReceipt {
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        PrintHeaders(output);
+        PrintReceiptHeaders(output);
 
         // prints lineItems
-        double totalSalesTax = 0d;
-        double totalAmount = 0d;
-        for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append('\t');
-            output.append(lineItem.getPrice());
-            output.append('\t');
-            output.append(lineItem.getQuantity());
-            output.append('\t');
-            output.append(lineItem.totalAmount());
-            output.append('\n');
-
+        AtomicReference<Double> totalSalesTax = new AtomicReference<>(0d);
+        AtomicReference<Double> totalAmount = new AtomicReference<>(0d);
+        order.getLineItems().forEach(lineItem -> {
+            printReceiptBody(output, lineItem);
             // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
-            totalSalesTax += salesTax;
-
+            totalSalesTax.updateAndGet(v -> v + salesTax);
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
-            totalAmount += lineItem.totalAmount() + salesTax;
-        }
-
+            totalAmount.updateAndGet(v -> v + lineItem.totalAmount() + salesTax);
+        });
         // prints the state tax
-        output.append("Sales Tax").append('\t').append(totalSalesTax);
+        output.append("Sales Tax").append('\t').append(totalSalesTax.get());
 
         // print total amount
-        output.append("Total Amount").append('\t').append(totalAmount);
+        output.append("Total Amount").append('\t').append(totalAmount.get());
         return output.toString();
     }
 
-    private void PrintHeaders(StringBuilder output) {
+    private void printReceiptBody(StringBuilder output, LineItem lineItem) {
+        output.append(lineItem.getDescription());
+        output.append('\t');
+        output.append(lineItem.getPrice());
+        output.append('\t');
+        output.append(lineItem.getQuantity());
+        output.append('\t');
+        output.append(lineItem.totalAmount());
+        output.append('\n');
+    }
+
+    private void PrintReceiptHeaders(StringBuilder output) {
         output.append("======Printing Orders======\n");
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
