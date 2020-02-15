6acdfb79ff43ce6411b224f3eb160ecdeb1f05f2
Zerong <zerong.ma@thoughtworks.com>
2020-02-15 20:19:41 +0800

    feat: just return 3 when I get the fibonacci number with position 4

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
@@ -4,6 +4,8 @@ public class FibonacciCalculator {
     public int getFibonacciByPosition(int position) {
         if (position == 3)
             return 2;
+        if (position == 4)
+            return 3;
         return 1;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculatorTest.java
@@ -22,4 +22,9 @@ public class FibonacciCalculatorTest {
     public void should_return_2_when_calculate_given_position_is_3() {
         assertEquals(2,fibonacciCalculator.getFibonacciByPosition(3));
     }
+    
+    @Test
+    public void should_return_3_when_calculate_given_position_is_4() {
+        assertEquals(3,fibonacciCalculator.getFibonacciByPosition(4));
+    }
 }
